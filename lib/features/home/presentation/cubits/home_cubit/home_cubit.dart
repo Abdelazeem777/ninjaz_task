ae5f84d7b7a3de13760dd643f2c94599fb7d0af2
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2024-03-08 18:34:20 +0200

    Add post loading and pagination logic to HomeCubit

--- lib/features/home/presentation/cubits/home_cubit/home_cubit.dart
+++ lib/features/home/presentation/cubits/home_cubit/home_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import '../../../../../../core/abstract/base_cubit.dart';
+import '../../../data/models/post_model.dart';
 import '../../../data/repositories/home_repository.dart';
 part 'home_state.dart';
 
@@ -10,4 +11,52 @@ class HomeCubit extends BaseCubit<HomeState> {
         super(const HomeState());
 
   final HomeRepository _homeRepository;
+
+  static const _postsLimit = 10;
+
+  Future<void> loadPosts([bool refresh = false]) async {
+    try {
+      if (!refresh) emit(state.copyWith(status: HomeStateStatus.loading));
+      final posts = await _homeRepository.getPosts(limit: _postsLimit);
+      emit(state.copyWith(
+        status: HomeStateStatus.loaded,
+        posts: posts,
+      ));
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: HomeStateStatus.error,
+          errorMessage: e.toString(),
+        ),
+      );
+    }
+  }
+
+  Future<void> refreshPosts() => loadPosts(true);
+
+  Future<void> loadMoreEvents() async {
+    if (state.isLoadingMore) return;
+    if ((state.posts ?? []).length < _postsLimit) return;
+
+    try {
+      emit(state.copyWith(status: HomeStateStatus.loadingMore));
+
+      final newPosts = await _homeRepository.getPosts(
+        page: (state.posts?.length ?? 0) ~/ _postsLimit,
+        limit: _postsLimit,
+      );
+
+      emit(
+        state.copyWith(
+          status: HomeStateStatus.loaded,
+          posts: [...?state.posts, ...newPosts],
+        ),
+      );
+    } catch (e) {
+      emit(state.copyWith(
+        status: HomeStateStatus.error,
+        errorMessage: e.toString(),
+      ));
+    }
+  }
 }
--- lib/features/home/presentation/cubits/home_cubit/home_state.dart
+++ lib/features/home/presentation/cubits/home_cubit/home_state.dart
@@ -3,6 +3,7 @@ part of 'home_cubit.dart';
 enum HomeStateStatus {
   initial,
   loading,
+  loadingMore,
   loaded,
   error,
 }
@@ -10,6 +11,7 @@ enum HomeStateStatus {
 extension HomeStateX on HomeState {
   bool get isInitial => status == HomeStateStatus.initial;
   bool get isLoading => status == HomeStateStatus.loading;
+  bool get isLoadingMore => status == HomeStateStatus.loadingMore;
   bool get isLoaded => status == HomeStateStatus.loaded;
   bool get isError => status == HomeStateStatus.error;
 }
@@ -18,10 +20,12 @@ extension HomeStateX on HomeState {
 class HomeState {
   final HomeStateStatus status;
   final String? errorMessage;
+  final List<PostModel>? posts;
 
   const HomeState({
     this.status = HomeStateStatus.initial,
     this.errorMessage,
+    this.posts,
   });
 
   @override
@@ -30,19 +34,23 @@ class HomeState {
 
     return other.runtimeType == runtimeType &&
         (other as HomeState).status == status &&
-        other.errorMessage == errorMessage;
+        other.errorMessage == errorMessage &&
+        listEquals(other.posts, posts);
   }
 
   @override
-  int get hashCode => status.hashCode ^ errorMessage.hashCode;
+  int get hashCode =>
+      status.hashCode ^ errorMessage.hashCode ^ Object.hashAll(posts ?? []);
 
   HomeState copyWith({
     HomeStateStatus? status,
     String? errorMessage,
+    List<PostModel>? posts,
   }) {
     return HomeState(
       status: status ?? this.status,
       errorMessage: errorMessage ?? this.errorMessage,
+      posts: posts ?? this.posts,
     );
   }
 }

1a52fa5c80abd4635a0517828f70620dc3cd11a6
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2024-03-08 18:25:15 +0200

    HomePage UI base is done

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,23 +1,58 @@
-
 import 'package:flutter/material.dart';
+import 'package:ninjaz_task/style/app_colors.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key
-  });
+  const HomePage({super.key});
   static const routeName = '/HomePage';
-
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: const Text('Home'),
-     ),
-     body: const Center(
-       child: Text('Home Page'),
-     ),
-   );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: _buildBottomNavigationBar(context),
+    );
   }
 
+  Widget _buildBody() {
+    return const Center(
+      child: Text('Home Page'),
+    );
+  }
+
+  Widget _buildBottomNavigationBar(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: AppColors.PRIMARY_COLOR,
+      selectedItemColor: AppColors.SECONDARY_COLOR,
+      onTap: (index) => _onTabChanged(context, index),
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Posts',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.tab),
+          label: 'Tab 2',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.tab),
+          label: 'Tab 3',
+        ),
+      ],
+    );
+  }
+
+  void _onTabChanged(BuildContext context, int index) {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+    scaffoldMessenger.removeCurrentSnackBar();
+    if (index == 1 || index == 2) {
+      scaffoldMessenger.showSnackBar(
+        const SnackBar(
+          content: Text('Coming soon!'),
+        ),
+      );
+    }
+  }
 }
